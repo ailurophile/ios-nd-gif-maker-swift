a1e78eece280f57af3e8460e9d75caed35446f3b
ailurophile <lisalitchfield@aim.com>
2017-06-06 13:08:52 -0700

    feat: Implement convertVideoToGif and displayGIF methods fix: change URLS to URL from NSURL to accommodate current Regift interface requirements

--- GifMaker_Swift_Template/GifEditorViewController.swift
+++ GifMaker_Swift_Template/GifEditorViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class GifEditorViewController: UIViewController {
 
+    @IBOutlet weak var gifImageView: UIImageView!
+    var gifURL: URL? = nil
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let gifURL = gifURL{
+            let gifFromRecording = UIImage.gif(url: gifURL.absoluteString)
+            gifImageView.image = gifFromRecording
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- GifMaker_Swift_Template/UIViewController+Record.swift
+++ GifMaker_Swift_Template/UIViewController+Record.swift
@@ -10,6 +10,10 @@ import Foundation
 import UIKit
 import MobileCoreServices
 
+//Regift constants
+let frameCount = 16
+let delayTime:Float = 0.2
+let loopCount = 0 // 0 means loop forever
 
 extension UIViewController{
     @IBAction func launchVideoCamera(sender: AnyObject){
@@ -25,6 +29,17 @@ extension UIViewController{
         return picker
         
     }
+    func convertVideoToGif(videoURL: URL){
+        let regift = Regift(sourceFileURL: videoURL as URL, frameCount: frameCount, delayTime: delayTime)
+        let gifURL = regift.createGif()
+        displayGIF(url: gifURL!)
+        
+    }
+    func displayGIF(url: URL){
+        let gifEditorVC = storyboard?.instantiateViewController(withIdentifier: "GifEditorViewController") as! GifEditorViewController
+        gifEditorVC.gifURL = url
+        navigationController?.pushViewController(gifEditorVC, animated: true)
+    }
 }
 //MARK: UIImagePickerControllerDelegate
 
@@ -36,9 +51,10 @@ extension UIViewController: UIImagePickerControllerDelegate{
         let mediaType = info[UIImagePickerControllerMediaType] as! String
         
         if mediaType == kUTTypeMovie as String{
-            let videoURL = info[UIImagePickerControllerMediaURL] as! NSURL
+            let videoURL = info[UIImagePickerControllerMediaURL] as! URL
+            convertVideoToGif(videoURL: videoURL )
 //            print(videoURL.path)
-            UISaveVideoAtPathToSavedPhotosAlbum(videoURL.path!, nil, nil, nil)
+//            UISaveVideoAtPathToSavedPhotosAlbum(videoURL.path!, nil, nil, nil)
             dismiss(animated: true, completion: nil)
             
         }
